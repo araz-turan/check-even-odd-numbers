a3db057612867fafe115aaff202a2e5955a1f450
Araz <122231385+araz-turan@users.noreply.github.com>
2024-10-27 00:17:15 +0330

    checking even odd numbers
    Detects whether the entered number is even or odd.

---
+++ check even number/script.js
@@ -0,0 +1,7 @@
+var entryNum = Number(prompt('Enter a number: ', [0]));
+// alert(typeof entryNum);
+if (entryNum % 2 == 0) {
+    alert('The entered number is even.');
+} else {
+    alert('The entered number is odd.');
+}
